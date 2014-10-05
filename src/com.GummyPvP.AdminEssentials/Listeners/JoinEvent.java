68c60dd8c077537f2b81875939fe201ac0529bbb
Googlelover1234 <GummyKits@yahoo.com>
2014-10-05 12:46:59 -0400

    Update JoinEvent.java

--- src/com.GummyPvP.AdminEssentials/Listeners/JoinEvent.java
+++ src/com.GummyPvP.AdminEssentials/Listeners/JoinEvent.java
@@ -1,1 +1,30 @@
-TODO - Add Event
+package com.GummyPvP.AdminEssentials.Listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import com.GummyPvP.AdminEssentials.Main.Main;
+
+public class JoinEvent implements Listener {
+
+	private Main plugin;
+
+	public JoinEvent(Main plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		for (Player admins : plugin.admin) {
+			if (p.hasPermission("adminessentials.seeadmins")) {
+				p.showPlayer(admins);
+			} else {
+				p.hidePlayer(admins);
+			}
+		}
+	}
+
+}
