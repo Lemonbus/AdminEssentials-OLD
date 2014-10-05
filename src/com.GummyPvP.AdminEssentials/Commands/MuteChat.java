0b1ad72fa6a79966c0afd224980e8ab2bbeac888
Googlelover1234 <GummyKits@yahoo.com>
2014-10-05 14:31:20 -0400

    Update MuteChat.java

--- src/com.GummyPvP.AdminEssentials/Commands/MuteChat.java
+++ src/com.GummyPvP.AdminEssentials/Commands/MuteChat.java
@@ -9,35 +9,41 @@ import org.bukkit.command.CommandSender;
 import com.GummyPvP.AdminEssentials.Main.Main;
 
 public class MuteChat implements CommandExecutor {
-	
+
 	private Main plugin;
-	
+
 	public MuteChat(Main plugin) {
 		this.plugin = plugin;
 	}
+
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("MuteChat")) {
-			if(args.length != 0) {
+	public boolean onCommand(CommandSender sender, Command cmd, String arg2,
+			String[] args) {
+		if (cmd.getName().equalsIgnoreCase("MuteChat")) {
+			if (args.length != 0) {
 				sender.sendMessage(ChatColor.RED + "/Mutechat");
 				return true;
 			}
-			if(plugin.Muted) {
-				plugin.Muted = false;
-				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes(
-						'&',
-						plugin.getConfig().getString("Settings.prefix")
-								+ plugin.getConfig()
-										.getString("Settings.unmutechat-message")
-										.replaceAll("{PLAYER}", sender.getName())));
-			} else {
+			if (!plugin.Muted) {
 				plugin.Muted = true;
 				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes(
 						'&',
 						plugin.getConfig().getString("Settings.prefix")
 								+ plugin.getConfig()
-										.getString("Settings.clearchat-message")
-										.replaceAll("{PLAYER}", sender.getName())));
+										.getString("Settings.mutechat-message")
+										.replaceAll("%PLAYER%",
+												sender.getName())));
+
+			} else {
+				plugin.Muted = false;
+				Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes(
+						'&',
+						plugin.getConfig().getString("Settings.prefix")
+								+ plugin.getConfig()
+										.getString(
+												"Settings.unmutechat-message")
+										.replaceAll("%PLAYER%",
+												sender.getName())));
 			}
 		}
 		return true;
