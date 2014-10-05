41117d0f96229d7773b5c23df49f579e14f28f86
Googlelover1234 <GummyKits@yahoo.com>
2014-10-05 13:39:21 -0400

    Create ChatEvent.java

---
+++ src/com.GummyPvP.AdminEssentials/Listeners/ChatEvent.java
@@ -0,0 +1,29 @@
+package com.GummyPvP.AdminEssentials.Listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import com.GummyPvP.AdminEssentials.Main.Main;
+
+public class ChatEvent implements Listener {
+
+	private Main plugin;
+
+	public ChatEvent(Main plugin) {
+		this.plugin = plugin;
+	}
+
+	public void onChat(AsyncPlayerChatEvent e) {
+		Player p = e.getPlayer();
+		if (plugin.Muted) {
+			p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin
+					.getConfig().getString("Settings.prefix")
+					+ plugin.getConfig().getString("Settings.chat-is-muted")));
+			e.setCancelled(true);
+		} else
+			return;
+	}
+
+}
